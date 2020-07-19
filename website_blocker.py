d3fcece5c6da8c5b2e5a33e52d552adec81c47f2
shriya2310 <34997044+shriya2310@users.noreply.github.com>
2020-07-19 19:42:48 +0530

    Add files via upload

---
+++ website_blocker.py
@@ -0,0 +1,26 @@
+'''to run it from terminal open terminal as administrator and change the host path'''
+
+import time
+from datetime import datetime as dt
+
+hosts_temp='hosts'
+hosts_path=r"C:\Windows\System32\drivers\etc\hosts"
+redirect="127.0.0.1"
+websites=["www.facebook.com","facebook.com"]
+
+while True:
+    if 8<=dt.now().hour<=16:
+        with open(hosts_temp,'r+') as file:
+            content=file.read()
+            for website in websites:
+                if website not in content:
+                    file.write(redirect+" "+website+"\n")
+    else:
+         with open(hosts_temp,'r+') as file:
+            content=file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in websites):
+                    file.write(line)
+            file.truncate()
+    time.sleep(5)
